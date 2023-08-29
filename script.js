e2983c21144ad02d68c9de1f1e4e1912214ff593
TITUS KILUNDI <muthiani42@gmail.com>
2023-08-29 14:36:19 +0300

    version 8

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
+// Create main container
 const mainContainer = document.createElement("div");
 mainContainer.setAttribute("class", "container-fluid mt-4");
 document.body.appendChild(mainContainer);
 
+// Create wrapper container
 const wrapperContainer = document.createElement("div");
 wrapperContainer.setAttribute("class", "wrapperContainer innerContainer");
 mainContainer.appendChild(wrapperContainer);
 
+// Create input container
 const inputContainer = document.createElement("div");
 inputContainer.setAttribute("class", "inputContainer innerContainer");
 wrapperContainer.appendChild(inputContainer);
 
-/* <<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>> */
-/* <<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+// Create city dataset array
 const cityDataset = [];
 
+// Function to fetch cities from API
 async function getCities() {
   try {
     const response = await fetch("https://api.teleport.org/api/urban_areas/");
     const data = await response.json();
-
     const cities = data["_links"]["ua:item"];
     const cityNames = cities.map((city) => city["name"]);
-
     cityDataset.push(...cityNames);
   } catch (error) {
-    console.error("An error occurred:", error);
+    // console.log.error("An error occurred:", error);
   }
 }
 
+// Function to create city input and datalist
 function createCityInputAndDatalist() {
-  //   const container = document.getElementById("container");
-
   const input = document.createElement("input");
   input.type = "text";
   input.id = "city-input";
@@ -39,7 +39,6 @@ function createCityInputAndDatalist() {
 
   const datalist = document.createElement("datalist");
   datalist.id = "city-datalist";
-
   cityDataset.forEach((cityName) => {
     const option = document.createElement("option");
     option.value = cityName;
@@ -48,14 +47,8 @@ function createCityInputAndDatalist() {
 
   inputContainer.appendChild(input);
   inputContainer.appendChild(datalist);
-  //   container.appendChild(input);
-  //   container.appendChild(datalist);
-
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>>>> */
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>>>> */
 
   const innerDiv1 = document.createElement("div");
-
   wrapperContainer.appendChild(innerDiv1);
 
   const emptyInputError = document.createElement("div");
@@ -63,25 +56,12 @@ function createCityInputAndDatalist() {
   wrapperContainer.appendChild(emptyInputError);
 
   const innerDiv2 = document.createElement("div");
-  // innerDiv2.setAttribute("class", "innerDiv2 innerContainer");
   wrapperContainer.appendChild(innerDiv2);
-
-  /*   const weatherIcon = document.createElement("img");
-  // weatherIcon.setAttribute("src", `${data.icon}`);
-
-  weatherIcon.setAttribute("alt", "Weather Icon");
-  innerDiv1.appendChild(weatherIcon); */
-
-  // continue creating new containers
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>>>> */
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>>>> */
 
   const searchBtn = document.createElement("button");
   searchBtn.setAttribute("class", "btn btn-primary searchBtn innerContainer");
   searchBtn.innerHTML = ` <p>Search</p> <p><i class="fa-solid fa-magnifying-glass fa-beat-fade fa-sm"></i></p> `;
   inputContainer.appendChild(searchBtn);
-
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>> */
 
   input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
@@ -89,7 +69,6 @@ function createCityInputAndDatalist() {
     }
   });
 
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>> */
   searchBtn.addEventListener("click", async () => {
     try {
       emptyInputError.innerHTML = "";
@@ -98,70 +77,44 @@ function createCityInputAndDatalist() {
 
       const selectedCity = input.value;
       if (selectedCity.trim() === "") {
-        // Handle the case where no value was entered
-
         const emptyError = document.createElement("p");
         emptyError.textContent = "Please enter a city name";
         emptyError.setAttribute("class", "emptyInput");
         emptyInputError.appendChild(emptyError);
-        console.log("Please enter a city name.");
+
         innerDiv1.innerHTML = "";
         innerDiv2.innerHTML = "";
         innerDiv1.removeAttribute("class");
         innerDiv2.removeAttribute("class");
         return;
       }
-      // clean the containers
 
       innerDiv1.innerHTML = "";
       innerDiv2.innerHTML = "";
 
-      // weatherIcon.innerHTML = "";
-
-      // openweathermap website
       const apiKey = "e8a6581a1cb32127d23b2ec3c22162a5";
       let cityName = input.value;
-      console.log(cityName);
 
       const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
-      console.log(apiurl);
 
-      // fetching data from the API and storing it in a variable called response
       const response = async () => {
         const res = await fetch(apiurl);
-        // console.log(res.status);
+
         if (res.status === 404) {
-          console.log("City not found.");
           innerDiv1.innerHTML = "";
           innerDiv2.innerHTML = "";
           innerDiv1.removeAttribute("class");
           innerDiv2.removeAttribute("class");
-
           const errorElement = document.createElement("p");
           errorElement.setAttribute("class", "error-message");
           errorElement.innerHTML = ` <span id="cityError">"${cityName}"</span>   is not a city. Enter a valid city name.`;
           emptyInputError.appendChild(errorElement);
-
           return;
         }
+
         const data = await res.json();
-        console.log(data);
-        console.log(data.sys.country);
 
-        console.log(data.name);
-        console.log(data.weather);
-        console.log(data.weather[0].main);
-        console.log(`id: ${data.weather[0].id}`);
-        console.log(`humidity: ${data.main.humidity}%`);
-        console.log(`pressure: ${data.main.pressure}`);
-        console.log(`temp: ${data.main.temp}`);
-        console.log(`wind deg: ${data.wind.deg}`);
-        console.log(`wind speed: ${data.wind.speed}km/h.`);
-        console.log(`longitude: ${data.coord.lon}`);
-        console.log(`latitude: ${data.coord.lat}`);
-
-        /* <<<<<<<<<<<>display Data start>>>>>>>>>>>>>< */
-        // displaying the data on the screen
+        /* Display Data Start */
         const element1 = document.createElement("div");
         element1.setAttribute("class", "element1 innerContainer");
         element1.innerHTML = `${data.weather[0].main}`;
@@ -169,10 +122,8 @@ function createCityInputAndDatalist() {
 
         const imageIcons = document.createElement("img");
         imageIcons.setAttribute("class", "imageIcons innerContainer");
-        // imageIcons.innerHTML = `images/Weather Icons/clear.svg`;
         element1.appendChild(imageIcons);
 
-        /* <<<<<<<<<<<<>>>>>time start>>>>>>><<<<<<<<< */
         const now = new Date();
         const currentHour = now.getHours();
         const currentMinute = now.getMinutes();
@@ -181,12 +132,11 @@ function createCityInputAndDatalist() {
         const formattedMinute =
           currentMinute < 10 ? "0" + currentMinute : currentMinute;
         const time = `${formattedHour}:${formattedMinute} ${period}`;
-        console.log(time);
 
         let timeOfDay;
+
         if (currentHour >= 6 && currentHour < 18) {
           timeOfDay = "daytime";
-          console.log(`Day time`);
 
           let id = data.weather[0].id;
 
@@ -207,7 +157,7 @@ function createCityInputAndDatalist() {
           }
         } else {
           timeOfDay = "nighttime";
-          console.log(`night time`);
+
           let id = data.weather[0].id;
 
           if (id === 800) {
@@ -226,25 +176,7 @@ function createCityInputAndDatalist() {
             imageIcons.src = `https://openweathermap.org/img/wn/50d.png`;
           }
         }
-        /* <<<<<<<<<<<<>>>>>time end>>>>>>><<<<<<<<< */
 
-        /*   let id = data.weather[0].id;
-
-        if (id === 800) {
-          imageIcons.src = `images/Weather Icons/clear.svg`;
-        } else if (id >= 801 && id <= 804) {
-          imageIcons.src = `images/Weather Icons/cloud.svg`;
-        } else if (id >= 500 && id <= 531) {
-          imageIcons.src = `images/Weather Icons/rain.svg`;
-        } else if (id >= 300 && id <= 321) {
-          imageIcons.src = `images/Weather Icons/drizzle.png`;
-        } else if (id >= 200 && id <= 232) {
-          imageIcons.src = `images/Weather Icons/storm.svg`;
-        } else if (id >= 600 && id <= 622) {
-          imageIcons.src = `images/Weather Icons/snow.svg`;
-        } else if (id >= 701 && id <= 781) {
-          imageIcons.src = `images/Weather Icons/haze.svg`;
-        } */
         const collectEl = document.createElement("div");
         collectEl.setAttribute("class", "collectEl innerContainer");
         innerDiv2.appendChild(collectEl);
@@ -269,16 +201,6 @@ function createCityInputAndDatalist() {
         element6.innerHTML = `wind speed: ${data.wind.speed} km/h.`;
         element5.appendChild(element6);
 
-        /*  const element7 = document.createElement("div");
-        element7.setAttribute("class", "element7 innerContainer");
-        element7.innerHTML = `longitude: ${data.coord.lon}`;
-        innerDiv2.appendChild(element7);
-
-        const element8 = document.createElement("div");
-        element8.setAttribute("class", "element8 innerContainer");
-        element8.innerHTML = `latitude: ${data.coord.lat}`;
-        innerDiv2.appendChild(element8); */
-
         const element9 = document.createElement("div");
         element9.setAttribute("class", "element9 innerContainer");
         element9.innerHTML = `Feels like: ${Math.floor(
@@ -286,31 +208,12 @@ function createCityInputAndDatalist() {
         )}°C`;
         element2.appendChild(element9);
 
-        /* const element10 = document.createElement("div");
-        element10.setAttribute("class", "element10 innerContainer");
-        element10.innerHTML = `<img src="images/Weather Icons/clear.svg" alt="clear" class="images" >
-        <img src="images/Weather Icons/cloud.svg" alt="cloud" class="images" >
-        <img src="images/Weather Icons/haze.svg" alt="haze" class="images" >
-        <img src="images/Weather Icons/rain.svg" alt="rain" class="images" >
-        <img src="images/Weather Icons/snow.svg" alt="snow" class="images" >
-        <img src="images/Weather Icons/storm.svg" alt="storm" class="images" >
-        `;
-        innerDiv2.appendChild(element10); */
-
-        // continue creating new elements
-        /* <<<<<<<<<<<>display Data end>>>>>>>>>>>>>< */
-
-        /* <<<<<<<<<<<>display city name start>>>>>>>>>>>>>< */
         const cityName1 = document.createElement("div");
         cityName1.setAttribute("class", "cityName1");
         cityName1.innerHTML = ` <div id="cityContainer"> <p id="cityLebal">City:</p> <p>${data.name}</p> </div> `;
         innerDiv1.appendChild(cityName1);
-        /* <<<<<<<<<<<>display city name end>>>>>>>>>>>>>< */
 
-        /* <<<<<<<<<>>>>>>>>>>><<<<<<<<<<<<>> */
-        /* <<<<<<<<<>>>>>>>>display country name start>>><<<<<<<<<<<<>> */
-
-        const countryCode = data.sys.country; // Replace with the desired alpha-2 country code
+        const countryCode = data.sys.country;
 
         async function getCountryFullName(code) {
           try {
@@ -318,11 +221,9 @@ function createCityInputAndDatalist() {
               `https://restcountries.com/v2/alpha/${code}`
             );
             const data = await response.json();
-
             if (response.ok) {
               return data.name;
             } else {
-              console.log("Country not found.");
               return null;
             }
           } catch (error) {
@@ -334,10 +235,6 @@ function createCityInputAndDatalist() {
         async function displayCountryFullName() {
           const fullName = await getCountryFullName(countryCode);
           if (fullName) {
-            console.log(
-              `Full name of country with code ${countryCode}: ${fullName}`
-            );
-
             const country = document.createElement("div");
             country.setAttribute("class", "country");
             innerDiv1.appendChild(country);
@@ -347,31 +244,23 @@ function createCityInputAndDatalist() {
           }
         }
 
-        // Call the function to display the full country name
         displayCountryFullName();
-
-        /* <<<<<<<<<>>>>>>>>display country name end>>><<<<<<<<<<<<>> */
-        /* <<<<<<<<<>>>>>>>>>>><<<<<<<<<<<<>> */
       };
-      response();
 
+      response();
       input.value = "";
     } catch (error) {
       console.error("An error occurred:", error);
       return [];
     }
   });
-
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>>>> */
-  /* <<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<>>>> */
 }
 
+// Function to setup the page
 async function setupPage() {
   await getCities();
   createCityInputAndDatalist();
 }
 
+// Call the setupPage function
 setupPage();
-
-/* <<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>> */
-/* <<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>> */
